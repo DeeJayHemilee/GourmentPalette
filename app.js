6942ac1cea615b552a210047bed3621a98208bba
DeeJayHemilee <damilolaemiola001@gmail.com>
2025-10-07 08:50:55 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+        // Mobile Menu Toggle
+        const menuToggle = document.getElementById('menuToggle');
+        const navMenu = document.getElementById('navMenu');
+        
+        menuToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+            menuToggle.innerHTML = navMenu.classList.contains('active') ? 
+                '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
+        });
+
+       
